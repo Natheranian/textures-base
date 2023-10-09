a7d9a5b20ae044484f7831ee4374681d0cbbae72
Nathan Tarrant <st20252897@outlook.cardiffmet.ac.uk>
2023-10-09 13:53:52 +0100

    Worshop Tasks Week 3

--- glDemo/main.cpp
+++ glDemo/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 // Example exture object
 GLuint playerTexture;
+GLuint brickTexture;
 
 
 // Window size
@@ -19,6 +20,9 @@ void renderScene();
 void resizeWindow(GLFWwindow* window, int width, int height);
 void keyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
 void updateScene();
+void drawL();
+void drawScales();
+void drawPolygon(int sides);
 
 
 int main() {
@@ -69,6 +73,7 @@ int main() {
 	// Setup textures
 	//
 
+	
 	// Load image file from disk
 	auto textureImageFile = string("Assets\\Textures\\player1_ship.png");
 	FIBITMAP* bitmap = FreeImage_Load(FIF_PNG, textureImageFile.c_str(), BMP_DEFAULT);
@@ -109,8 +114,47 @@ int main() {
 		cout << "Error loading image!" << endl;
 	}
 
+	
+	//Load image file from disk
+	textureImageFile = string("Assets\\Textures\\brick2.png");
+	bitmap = FreeImage_Load(FIF_PNG, textureImageFile.c_str(), BMP_DEFAULT);
 
+	if (bitmap) {
 
+		// If image loaded, setup new texture object in OpenGL
+		glGenTextures(1, &brickTexture); // can create more than 1!
+
+		if (brickTexture) {
+
+			glBindTexture(GL_TEXTURE_2D, brickTexture);
+
+			// Setup texture image properties
+			glTexImage2D(
+				GL_TEXTURE_2D,
+				0,
+				GL_RGBA,
+				FreeImage_GetWidth(bitmap),
+				FreeImage_GetHeight(bitmap),
+				0,
+				GL_RGB,
+				GL_UNSIGNED_BYTE,
+				FreeImage_GetBits(bitmap));
+
+			// Setup texture filter and wrap properties
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		}
+
+		// Once the texture has been setup, the image data is copied into OpenGL.  We no longer need the originally loaded image
+		FreeImage_Unload(bitmap);
+	}
+	else {
+
+		cout << "Error loading image!" << endl;
+	}
+	
 	//
 	// 2. Main loop
 	// 
@@ -138,9 +182,10 @@ int main() {
 // renderScene - function to render the current scene
 void renderScene()
 {
+	
 	// Clear the rendering window
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+	/*
 	// Render objects here...
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, playerTexture);
@@ -163,7 +208,108 @@ void renderScene()
 
 	glDisable(GL_BLEND);
 	glDisable(GL_TEXTURE_2D);
+	*/
+	drawScales();
 }
+
+void drawL()
+{
+	glBegin(GL_TRIANGLE_FAN);
+
+	glVertex2f(-0.8f, -0.6f);
+	glVertex2f(-0.8f, 0.6f);
+	glVertex2f(-0.5f, 0.6f);
+
+	glVertex2f(-0.5f, -0.3f);
+	glVertex2f(0.6f, -0.3f);
+	glVertex2f(0.6f, -0.6f);
+
+	glEnd();
+}
+
+void drawScales()
+{
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, brickTexture);
+
+	glBegin(GL_TRIANGLES);
+
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2f(-0.3f, -1.0f);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex2f(0.3f, -1.0f);
+	glTexCoord2f(0.5f, 1.0f);
+	glVertex2f(0.0f, 0.5f);
+
+	glVertex2f(-0.5f, 0.6f);
+	glVertex2f(0.5f, 0.6f);
+	glVertex2f(0.0f, 0.5f);
+
+	glVertex2f(-0.5f, 0.6f);
+	glVertex2f(0.5f, 0.6f);
+	glVertex2f(0.0f, 0.7f);
+
+	glEnd();
+
+	glDisable(GL_BLEND);
+	glDisable(GL_TEXTURE_2D);
+
+	glBegin(GL_LINE_LOOP);
+
+	glVertex2f(-0.5f, 0.6f);
+	glVertex2f(-0.7f, 0.2f);
+	glVertex2f(-0.3f, 0.2f);
+
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+
+	glVertex2f(0.5f, 0.6f);
+	glVertex2f(0.7f, 0.2f);
+	glVertex2f(0.3f, 0.2f);
+
+	glEnd();
+
+	glBegin(GL_QUADS);
+
+	glVertex2f(0.7f, -0.1f);
+	glVertex2f(0.7f, 0.2f);
+	glVertex2f(0.3f, 0.2f);
+	glVertex2f(0.3f, -0.1f);
+
+	glVertex2f(-0.7f, -0.1f);
+	glVertex2f(-0.7f, 0.2f);
+	glVertex2f(-0.3f, 0.2f);
+	glVertex2f(-0.3f, -0.1f);
+
+	glEnd();
+
+}
+
+void drawPolygon(int sides)
+{
+	glShadeModel(GL_SMOOTH);
+
+	const float pi = 3.1415926f;
+	const float thetaStep = 2.0f * pi / float(sides);
+
+	glBegin(GL_TRIANGLE_FAN);
+
+	glVertex2f(0.0f, 0.0f);
+
+	for (int i = 0; i <= sides; i++)
+	{
+
+		float x = cosf(thetaStep * float(i));
+		float y = sinf(thetaStep * float(i));
+
+		glVertex2f(x, y);
+
+	};
+
+	glEnd();
+
+};
 
 
 // Function to call when window resized
